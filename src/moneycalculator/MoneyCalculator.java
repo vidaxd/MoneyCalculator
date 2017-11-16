85c7c44126979edecf93e94d0da07c7db44f6721
Usuario <Usuario@DESKTOP-MAOPI2H>
2017-11-16 14:26:21 +0000

    Se crea un programa en el que se introduce una cantidad de dolares y se convierte a euros usando un servicio web.
    En esta version se le añade el modelo CIPO (Control-Input-Process-Output)
    
    Se modifica el programa para permitir introducir una divisa inicial
    para hallar su equivalente a Euros

--- src/moneycalculator/MoneyCalculator.java
+++ src/moneycalculator/MoneyCalculator.java
@@ -15,6 +15,7 @@ public class MoneyCalculator {
     
     private double amount;
     private double exchangeRate; 
+    private String currencyFrom;
     
     private void execute() throws Exception {
         input();
@@ -24,17 +25,20 @@ public class MoneyCalculator {
     
 
     private void input() {
-        System.out.println("Introduzca una cantidad de dolares");
+        System.out.println("Introduzca una cantidad");
         Scanner scanner = new Scanner(System.in);
         amount = Double.parseDouble(scanner.next());   
+        
+        System.out.println("Introduzca divisa inicial");
+        currencyFrom = scanner.next();
     }
 
     private void process() throws Exception {
-        exchangeRate = getExchangeRate("USD", "EUR");
+        exchangeRate = getExchangeRate(currencyFrom, "EUR");
     }
 
     private void output() {
-        System.out.println(amount+" USD equivalen a "+ amount*exchangeRate+"EUR");
+        System.out.println(amount + " " + currencyFrom + " equivalen a " + amount * exchangeRate + " EUR");
     }
     
     private static double getExchangeRate(String from, String to) throws Exception{
